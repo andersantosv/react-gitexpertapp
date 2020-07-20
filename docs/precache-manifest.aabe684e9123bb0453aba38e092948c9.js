eaa1dcbf652e1aba54c0e8329468f849fe520e1f
Anderson Santos <anderson.santos@oechsle.pe>
2020-07-19 20:17:34 -0500

    Docs added

---
+++ docs/precache-manifest.aabe684e9123bb0453aba38e092948c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c16da1ffa22fa7a975b38d5833be7916",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbb6a1f09da82ba30530",
+    "url": "/static/css/main.1e32b1ae.chunk.css"
+  },
+  {
+    "revision": "7df93c7276edf1b25086",
+    "url": "/static/js/2.65d56cdc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.65d56cdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbb6a1f09da82ba30530",
+    "url": "/static/js/main.461547f5.chunk.js"
+  },
+  {
+    "revision": "9d3929294d677b1b9ff6",
+    "url": "/static/js/runtime-main.aa9df6a3.js"
+  }
+]);
